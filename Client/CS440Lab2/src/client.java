33029a76a985f657bff8dad4fa5e2380f95b5d92
Angel Galeana <angel.galeanajr@siu.edu>
2019-04-04 09:42:21 -0500

    Fixed Code

--- Client/CS440Lab2/src/client.java
+++ Client/CS440Lab2/src/client.java
@@ -1,21 +1,60 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
-public class client {
+public class client implements Runnable {
 
-	public static void main(String[] args) throws IOException {
-		Socket s = new Socket("localhost", 12252);
+	private Socket s;
+	
+	public client() throws IOException {
+		s = new Socket("localhost", 12252);
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+		(new Thread(this)).start();
+		ReadClient(pr);
+	}
+	
+	public void ReadClient(PrintWriter pr) {
+		Scanner scan = new Scanner(System.in);
+		while(!s.isClosed()){
+			String line = scan.nextLine();
+			pr.println(line);
+			pr.flush();
+		}
+	}
+	
+	public void run() {
+		while(!s.isClosed()){
+			try {
+			InputStreamReader in = new InputStreamReader(s.getInputStream());
+			BufferedReader bf = new BufferedReader(in);
+			String str = bf.readLine();
+			System.out.println(str);
+			}catch(IOException e) {
+				System.err.println("Could not send message");
+			}
+			
+		}
 		
+	}
+
+	/*public static void main(String[] args) throws IOException {
+		Socket s = new Socket("localhost", 12252);
+		//in runnable thread, include scanner
 		PrintWriter pr = new PrintWriter(s.getOutputStream());
 		pr.println("Hello from Client1");
 		pr.flush();
-		
+		//while loop Main Thread
 		InputStreamReader in = new InputStreamReader(s.getInputStream());
 		BufferedReader bf = new BufferedReader(in);
 		
 		String str = bf.readLine();
-		System.out.println("Server : "+ str);
+		System.out.println(str); //server will send string and tell you where it came from
 
+	}**/
+	
+	public static void main(String[] args) throws IOException {
+	
+		client cl1 = new client();
 	}
 
 }
